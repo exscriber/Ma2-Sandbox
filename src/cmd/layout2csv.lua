e3b6e8b1042cae38e50ea472e0a7ae779b8619a1
Nick Zinovenko <8070617+exscriber@users.noreply.github.com>
2022-06-29 23:43:34 +0300

    layout2csv refactor

--- src/cmd/layout2csv.lua
+++ src/cmd/layout2csv.lua
@@ -10,55 +10,67 @@ local template = require 'pl.template'
 -- local pprint = require 'pl.pretty'.debug
 
 
-local function parsePatchXML(filename)
-    local xml = utils.readfile(filename)
+local function readPatchXML(filename)
+    local xml = utils.readfile(filename) ---@type string|nil
 
     local result = {}
-    for fix_id, fix_data in xml:gmatch('<Fixture.-fixture_id="(%d+)".->(.-)</Fixture>') do
-        for sub_id, sub_data in fix_data:gmatch('<SubFixture.-index="(%d+)".->(.-)</SubFixture>') do
-            local address = sub_data:match('<Address>(%d+)</Address>')
+    for fix_attr, fix_data in xml:gmatch('<Fixture(.-)>(.-)</Fixture>') do
+        for sub_attr, sub_data in fix_data:gmatch('<SubFixture(.-)>(.-)</SubFixture>') do
+            -- id string '123.45' need 1 base correction in patch.xml
+            local fix_id = fix_attr:match('fixture_id="(%d+)"')
+                        or fix_attr:match('channel_id="(%d+)"')
+            local sub_id = sub_attr:match('index="(%d+)"') or 0
+            local id = fix_id .. '.' .. sub_id + 1
 
-            local id = fix_id .. '.' .. sub_id + 1 -- id string 123.45 with 1 base correction
-            result[id] = address + 512 -- absolute address with 1 base correction
+            -- Universe and Channel from absolute address
+            local address  = sub_data:match('<Address>(%d+)</Address>')
+            local universe = 1+ (address-1) // 512
+            local channel  = 1+ (address-1) %  512
+
+            result[id] = { universe, channel }
         end
     end
     return result
 end
 
-local function parseLayoutXML(filename, patch)
-    local xml = utils.readfile(filename)
+
+local function readLayoutXML(filename, patch)
+    local xml = utils.readfile(filename) ---@type string|nil
 
     local result = {}
-    for item, fixture in xml:gmatch('<LayoutSubFix (.-)>(.-)</LayoutSubFix>') do
-
-        -- #id string 123.45 already 1 based
-        local fix_id = fixture:match('fix_id="(%d+)')
-        local sub_id = fixture:match('sub_index="(%d+)"')
+    for attr, data in xml:gmatch('<LayoutSubFix(.-)>(.-)</LayoutSubFix>') do
+        -- id string '123.45' already 1 based in layout.xml
+        local fix_id = data:match('fix_id="(%d+)"')
+                    or data:match('cha_id="(%d+)"')
+        local sub_id = data:match('sub_index="(%d+)"') or 1
         local id = fix_id .. '.' .. sub_id
 
-        -- Universe and Channel from absolute address
-        local address  = patch[id]
-        local universe = address // 512
-        local channel  = address % 512
+        local universe, channel
+        local address = patch[id]
+        if address then
+            universe, channel = table.unpack(address)
+        else goto continue end -- no patch for item - next please...
 
         if not result[universe] then
             result[universe] = {}
         end
 
-        local attrib = {}
-        for k, v in item:gmatch('([%w_]-)="(.-)"') do
-            attrib[k] = v
+        local item = {}
+        for k,v in attr:gmatch('([%w_]-)="(.-)"') do
+            item[k] = v
         end
 
         table.insert(result[universe], {
             id   = id,
             addr = { u = universe, c = channel },
-            pos  = { x = attrib.center_x, y = attrib.center_y },
-            size = { w = attrib.size_w, h = attrib.size_h },
+            pos  = { x = item.center_x, y = item.center_y },
+            size = { w = item.size_w, h = item.size_h },
         })
+    ::continue::
     end
     return result
 end
+
 
 -- if __name__ == '__main__'
 if not debug.getinfo(3) then
@@ -67,15 +79,16 @@ if not debug.getinfo(3) then
     parser:argument('layout_file', 'MA2 layout xml file')
     local args = parser:parse()
 
-    local patch  = parsePatchXML(args.patch_file)
-    local layout = parseLayoutXML(args.layout_file, patch)
+    local patch  = readPatchXML(args.patch_file)
+    local layout = readLayoutXML(args.layout_file, patch)
 
     local csv = template.compile(
         "{id},\t{addr.u},{addr.c},\t{pos.x}, {pos.y},\t{size.w}, {size.h}",
         { inline_escape = '', inline_brackets = '{}' })
 
-    for _, universe in ipairs(layout) do
-        for _, item in ipairs(universe) do
+    for universe, items in ipairs(layout) do
+        print('Universe: ' .. universe)
+        for _, item in ipairs(items) do
             print(tostring(csv:render(item)))
         end
     end
--- src/cmd/layout2csv.py
+++ src/cmd/layout2csv.py
@@ -4,41 +4,52 @@
 # Copyright (c) 2022 Nick N. Zinovenko
 
 from xml.etree import ElementTree
-from pprint import pprint
+# from pprint import pprint
 
 # MA2 namespace prefix for XML parser
 MA2 = { '': 'http://schemas.malighting.de/grandma2/xml/MA' }
 
+# Types
+FixId = str
+Address = tuple[int,int]
+UniverseId = int
+LayoutItem = dict
 
-def parsePatchXML (filename):
+
+def readPatchXML (filename) -> dict[FixId, Address]:
     xml = ElementTree.parse(filename)
 
     result = {}
     for fixture in xml.iterfind('.//Fixture', MA2):
-        for subfixture in fixture.iterfind('SubFixture', MA2):
-            fix_id  = int( fixture.get('fixture_id') )
-            sub_id  = int( subfixture.get('index') )
-            address = int( subfixture.find('*Address', MA2).text )
+        for subfixt in fixture.iterfind('SubFixture', MA2):
+            # id string '123.45' need 1 base correction in patch.xml
+            fix_id = fixture.get('fixture_id') or fixture.get('channel_id')
+            sub_id = subfixt.get('index', '0') # 0 based
+            id = f'{fix_id}.{int(sub_id) +1}'
 
-            id = f'{fix_id}.{sub_id +1}'    # id string 123.45 with 1 base correction
-            result[id] = address +512       # absolute address with 1 base correction
+            # Universe and Channel from absolute address
+            address  = int( subfixt.find('*Address', MA2).text )
+            universe = 1+ (address-1) // 512
+            channel  = 1+ (address-1) %  512
+
+            result[id] = (universe, channel)
     return result
 
 
-def parseLayoutXML (filename, patch):
+def readLayoutXML (filename, patch: dict[FixId, Address] ) -> dict[UniverseId,list[LayoutItem]]:
     xml = ElementTree.parse(filename)
 
     result = {}
-    for item in xml.iterfind('.//LayoutSubFix', MA2):        
+    for item in xml.iterfind('.//LayoutSubFix', MA2):
         if (fixture := item.find('Subfixture', MA2)) is not None:
+            # id string '123.45' already 1 based in layout.xml
+            fix_id = fixture.get('fix_id') or fixture.get('cha_id')
+            sub_id = fixture.get('sub_index', '1') # 1 based
+            id = f'{fix_id}.{sub_id}'
 
-            #id string 123.45 already 1 based
-            id = '{fix_id}.{sub_index}'.format(**fixture.attrib)
-
-            # Universe and Channel from absolute address
-            address  = patch[id]        
-            universe = address // 512
-            channel  = address % 512
+            if address := patch.get(id):
+                universe, channel = address
+            else: continue # no patch for item - next please...
 
             if universe not in result:
                 result[universe] = []
@@ -60,11 +71,12 @@ if __name__ == '__main__':
     parser.add_argument('layout_file', help='MA2 layout xml file')
     args = parser.parse_args()
 
-    patch  = parsePatchXML( args.patch_file )
-    layout = parseLayoutXML( args.layout_file, patch )
+    patch  = readPatchXML( args.patch_file )
+    layout = readLayoutXML( args.layout_file, patch )
 
     csv = "{id},\t{addr[u]},{addr[c]},\t{pos[x]}, {pos[y]},\t{size[w]}, {size[h]}"
-    
-    for universe in layout.values():
-        for item in universe:
+
+    for universe,items in layout.items():
+        print (f'Universe: {universe}')
+        for item in items:
             print (csv.format(**item))
