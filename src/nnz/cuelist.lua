809ec21406b2594a686f3060f131ed805c8124b5
Nick Zinovenko <8070617+exscriber@users.noreply.github.com>
2022-09-30 17:12:21 +0300

    wip

---
+++ src/nnz/cuelist.lua
@@ -0,0 +1,22 @@
+-- NNZ CueList library for GrandMA2 Lua plugins
+--
+-- This code is freely distributable under the terms of the [MIT license]
+-- Copyright (c) 2022 Nick N. Zinovenko
+
+
+local _M = { _VERSION = '1.0' }
+
+
+---parse CueList text data to structured table
+---@param data string
+---@return table
+function _M.parseCueList(data)
+    local result = {}
+
+
+
+
+    return result
+end
+
+return _M
